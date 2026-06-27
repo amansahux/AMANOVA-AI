02e8ba4750315fc01fd3dcd2f27e47a3cc393fc0
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-06-27 20:06:57 +0530

    feat: initialize backend project with Express, Mongoose, and user authentication structure

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+app.use(express.json())
 
 app.get("/api", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
+
+//Routes
+
+app.use("/api/auth", authRouter)
 
 export default app;
