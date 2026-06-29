0be346e43082f8c7536db6b18f075ec3048c0c0f
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-06-29 20:30:11 +0530

    feat: initialize HTTP server and Socket.io setup with base dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
 import app from "./src/app.js";
 import config from "./src/config/config.js";
 import connectToDb from "./src/config/db.js";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
-app.listen(config.port, () => {
+const httpServer = http.createServer(app);
+
+initSocket(httpServer)
+
+connectToDb();
+
+httpServer.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
 });
-
-connectToDb()
-
---
+++ Backend/src/sockets/server.socket.js
@@ -0,0 +1,29 @@
+import config from "../config/config.js";
+import { Server } from "socket.io";
+
+let io;
+export function initSocket(httpServer) {
+  io = new Server(httpServer, {
+    cors: {
+      origin: config.base_url,
+      Credential: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("New Connection Established " + socket.id);
+
+    socket.on("disconnect", () => {
+      console.log(`User Disconnected: ${socket.id}`);
+    });
+  });
+}
+
+console.log("Socket Server is RUNNING");
+
+export function GetIO() {
+  if (!io) {
+    throw new Error("Socket.IO not initialized");
+  }
+  return io;
+}
